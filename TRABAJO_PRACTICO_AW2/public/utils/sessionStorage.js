79784a72ce4f33bded3e1b44c862e417dbf10286
martindiaz14 <144454009+martindiaz14@users.noreply.github.com>
2024-07-04 14:43:39 -0300

    Delete TRABAJO_PRACTICO_AW2 directory

--- TRABAJO_PRACTICO_AW2/public/utils/sessionStorage.js
+++
@@ -1,7 +0,0 @@
-export const addSession = (user)=>{
-    sessionStorage.setItem('user', JSON.stringify(user))
-}
-
-export const getSession = () =>{
-    return JSON.parse(sessionStorage.getItem('user'))
-}
--- TRABAJO_PRACTICO_AW2/utils/user.js
+++
@@ -1,9 +0,0 @@
-import{readFile} from 'fs/promises'
-
-
-const fileUser = await readFile('./DATA/usuarios.json', 'utf-8')
-const UserData = JSON.parse(fileUser);
-
-export const get_user_byId = (id)=>{
-     return UserData.find(e=> e.id === id)
-}
